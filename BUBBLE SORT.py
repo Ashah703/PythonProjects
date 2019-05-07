8f69017c91ebe3a72d983c5a601e9b487b425b5f
Akash Shah <akashshah703@gmail.com>
2019-05-07 10:56:07 -0500

    Add files via upload

---
+++ BUBBLE SORT.py
@@ -0,0 +1,23 @@
+"""
+@author: Akash Shah 
+Bubble Sort
+"""
+
+def BubbleSort(lst):
+    for i in range(len(lst) - 1, 0, -1): # Analyze starting from last
+        for j in range(i):
+            if(lst[j]> lst[j + 1]):
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return(lst)
+
+def main():
+    items = int(input('How many elements are in your list? '))
+    lst = []
+    for i in range(items):
+        num = int(input("Please enter integer: "))
+        lst.append(num)
+
+    print(BubbleSort(lst))
+    
+if __name__ == '__main__':
+    main()
