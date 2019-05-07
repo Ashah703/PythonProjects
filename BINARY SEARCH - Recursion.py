9a78ebd9ddcc77a15ea66bc95efb3104b4daf028
Akash Shah <akashshah703@gmail.com>
2019-05-07 10:57:19 -0500

    Add files via upload

---
+++ BINARY SEARCH - Recursion.py
@@ -0,0 +1,27 @@
+"""
+
+@author: Akash 
+Binary Search - recursion
+"""
+
+def binarySearch(lst, key):
+    # Base Case
+    if len(lst) == 1:
+        if lst[0] == key:
+            return(True)
+        return(False)
+
+    else:
+        middle = int(len(lst)//2)
+        # Looks at the second half
+        if lst[middle] < key:
+            return(binarySearch(lst[middle:],key))
+        # Looks at the first half
+        elif lst[middle] > key:
+            return(binarySearch(lst[:middle],key))
+        # If found, return True
+        return(True)
+
+lst = [1,2,4,5,6,7,88,979,7655]
+key = 999
+print(binarySearch(lst, key))
